3f5df243a1b03293902e9ff185e6beaa18bb508b
UMU618 <umu618@hotmail.com>
2019-10-18 17:48:28 +0800

    * Add table rows monitoring

--- index.js
+++ index.js
@@ -10,12 +10,16 @@
 const ME = 'eos-accounts-monitor'
 
 const assert = require('assert')
+const debug = require('debug')(ME)
 const os = require('os')
 
 const conf = require('./conf')
 assert(conf.url)
 const accounts = require('./accounts')
-assert(accounts.length)
+const tables = require('./tables')
+assert(accounts.length || tables.length)
+debug('accounts.length =', accounts.length)
+debug('tables.length =', tables.length)
 const alerts = require('./utils/alerts')
 
 const { JsonRpc } = require('eosjs')
@@ -25,48 +29,111 @@ const rpc = new JsonRpc(conf.url, { fetch })
 let index = 0
 let retry = 0
 
-main()
+let infoMessage = ''
+let errorMessage = ''
 
-function main() {
+queryAccounts()
+
+function getTableRows(code, scope, table, key) {
+  return rpc.get_table_rows({
+    json: true, code: code, scope: scope
+    , table: table, lower_bound: key, upper_bound: key, limit: 1
+  })
+}
+
+function queryAccounts() {
+  if (index >= accounts.length) {
+    index = 0
+    queryTables()
+    return
+  }
+
   const ac = accounts[index]
   rpc
     .get_account(ac.account)
     .then(res => {
-      // console.log(res.account_name, '"' + res.core_liquid_balance +'"'
-      //   , res.cpu_limit.available, res.ram_quota - res.ram_usage)
+      debug(res.account_name, '"' + res.core_liquid_balance +'"'
+        , res.cpu_limit.available, res.ram_quota - res.ram_usage)
       retry = 0
       if (res) {
         if (ac.balance && parseFloat(res.core_liquid_balance) < ac.balance) {
-          alerts.sendDingtalk(conf.dingtalkInfoToken
-            , (new Date).toJSON() + ', ' + os.hostname() + ', ' + ME + ': '
-            + ac.account + ' balance is not enough.')
+          infoMessage += '\n  ' + ac.account + ' balance is less then'
+            + ac.balance + ' EOS.'
         }
         if (ac.cpu && res.cpu_limit.available < ac.cpu) {
-          alerts.sendDingtalk(conf.dingtalkInfoToken
-            , (new Date).toJSON() + ', ' + os.hostname() + ', ' + ME + ': '
-            + ac.account + ' CPU is not enough.')
+          infoMessage += '\n  ' + ac.account + ' CPU is less then' + ac.cpu
+            + 'us.'
         }
         if (ac.ram && res.ram_quota - res.ram_usage < ac.ram) {
-          alerts.sendDingtalk(conf.dingtalkInfoToken
-            , (new Date).toJSON() + ', ' + os.hostname() + ', ' + ME + ': '
-            + ac.account + ' RAM is not enough.')
+          infoMessage += '\n  ' + ac.account + ' RAM is less then' + ac.ram
+            + 'bytes.'
         }
       }
 
       ++index
-      if (index < accounts.length) {
-        main()
-      }
+      queryAccounts()
     })
     .catch(e => {
       console.error(JSON.stringify(e.json, null, 2))
       ++retry
       if (retry <= conf.maxRetry) {
-        main()
+        queryAccounts()
       } else {
-        alerts.sendDingtalk(ERROR_TOKEN
-          , (new Date).toJSON() + ', ' + os.hostname() + ', ' + ME + ': query '
-          + ac.account + ' failed for ' + conf.maxRetry + ' times.')
+        errorMessage += '\n  query ' + ac.account + ' failed for '
+          + conf.maxRetry + ' times.'
+        queryTables()
       }
     })
+}
+
+function queryTables() {
+  if (index >= tables.length) {
+    notify()
+    return
+  }
+
+  const t = tables[index]
+  getTableRows(t.code, t.scope, t.table, t.key)
+    .then(res => {
+      retry = 0
+      if (res && res.rows && res.rows[0] && res.rows[0][t.rowKey]) {
+        debug(t.code + '/' + t.scope + '/' + t.table + '/'
+          + t.key + '/' + t.rowKey, '=', res.rows[0][t.rowKey])
+        const v = parseFloat(res.rows[0][t.rowKey])
+        if (v < parseFloat(t.rowValue)) {
+          infoMessage += '\n  ' + t.code + '/' + t.scope + '/' + t.table + '/'
+            + t.key + '/' + t.rowKey + ' is less then ' + t.rowValue + '.'
+        }
+      }
+
+      ++index
+      queryTables()
+    })
+    .catch(e => {
+      console.error(JSON.stringify(e.json, null, 2))
+      ++retry
+      if (retry <= conf.maxRetry) {
+        queryTables()
+      } else {
+        errorMessage += '\n  query ' + t.code + '/' + t.scope + '/' + t.table
+          + '/' + t.key + ' failed for ' + conf.maxRetry + ' times.'
+        notify()
+      }
+    })
+}
+
+function notify() {
+  if (infoMessage) {
+    alerts.sendDingtalk(conf.dingtalkInfoToken
+      , (new Date).toJSON() + ', ' + os.hostname() + ', ' + ME + ':'
+      + infoMessage)
+  }
+
+  if (errorMessage) {
+    alerts.sendDingtalk(conf.dingtalkErrorToken
+      , (new Date).toJSON() + ', ' + os.hostname() + ', ' + ME + ':'
+      + errorMessage)
+  }
+
+  debug('end')
 }
